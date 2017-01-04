e9a08aec3f39a69fab50396fe037acaa9f4d590a
FrankWww <1245276385@qq.com>
2017-01-04 12:45:14 +0800

    用容器实现可扩展性

--- src/castle/Room.java
+++ src/castle/Room.java
@@ -1,27 +1,19 @@
 package castle;
 
+import java.util.HashMap;
+
 public class Room {
     public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+    private HashMap<String, Room> exits = new HashMap<String, Room>();
 
     public Room(String description) 
     {
         this.description = description;
     }
 
-    public void setExits(Room north, Room east, Room south, Room west) 
+    public void setExit(String dir, Room room) 
     {
-        if(north != null)
-            northExit = north;
-        if(east != null)
-            eastExit = east;
-        if(south != null)
-            southExit = south;
-        if(west != null)
-            westExit = west;
+        exits.put(dir, room);
     }
 
     @Override
@@ -32,31 +24,14 @@ public class Room {
     
     public String getExitDesc() {
     	StringBuffer sb = new StringBuffer();
-    	if( northExit != null )
-    		sb.append("north ");
-    	if( eastExit != null )
-    		sb.append("east ");
-    	if( westExit != null )
-    		sb.append("west ");
-    	if( southExit != null )
-    		sb.append("south ");   	
+    	for( String dir : exits.keySet() ) {
+    		sb.append(dir);
+    		sb.append(' ');
+    	}
     	return sb.toString();
     }
     
     public Room getExit(String direction) {
-    	Room ret = null;
-    	if(direction.equals("north")) {
-            ret = northExit;
-        }
-        if(direction.equals("east")) {
-            ret = eastExit;
-        }
-        if(direction.equals("south")) {
-            ret = southExit;
-        }
-        if(direction.equals("west")) {
-            ret = westExit;
-        }
-        return ret;
+        return exits.get(direction);
     }
 }
