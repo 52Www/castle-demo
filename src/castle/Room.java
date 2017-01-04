f3528729c7f2813059344fa3c2b9170c4cac27a4
FrankWww <1245276385@qq.com>
2017-01-04 12:30:27 +0800

    用封装来降低耦合

--- src/castle/Room.java
+++ src/castle/Room.java
@@ -29,4 +29,34 @@ public class Room {
     {
         return description;
     }
+    
+    public String getExitDesc() {
+    	StringBuffer sb = new StringBuffer();
+    	if( northExit != null )
+    		sb.append("north ");
+    	if( eastExit != null )
+    		sb.append("east ");
+    	if( westExit != null )
+    		sb.append("west ");
+    	if( southExit != null )
+    		sb.append("south ");   	
+    	return sb.toString();
+    }
+    
+    public Room getExit(String direction) {
+    	Room ret = null;
+    	if(direction.equals("north")) {
+            ret = northExit;
+        }
+        if(direction.equals("east")) {
+            ret = eastExit;
+        }
+        if(direction.equals("south")) {
+            ret = southExit;
+        }
+        if(direction.equals("west")) {
+            ret = westExit;
+        }
+        return ret;
+    }
 }
